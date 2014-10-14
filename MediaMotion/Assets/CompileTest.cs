0e3338a9003e10f8417f7318a99508704ffb111d
Quentin Barate <barate.q@gmail.com>
2014-10-14 12:30:26 +0100

    Compilation fixed.

---
+++ MediaMotion/Assets/CompileTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class CompileTest : MonoBehaviour
+{
+    Controller controller;
+
+    void Start()
+    {
+        controller = new Controller();
+    }
+
+    void Update()
+    {
+        Frame frame = controller.Frame();
+        if (frame.Fingers.Count > 0) {
+			System.Console.Write ("LeapTest");
+		}
+    }
+}
