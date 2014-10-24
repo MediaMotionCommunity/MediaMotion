80eedec6e879bf271abaa2cc66097896adbdde46
perra_l <luca.perra@epitech.eu>
2014-10-24 15:31:18 +0100

    adding controller & view interfaces

---
+++ MediaMotion/Assets/Scripts/View/IFolderUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using MediaMotion.View;
+
+public interface IFolderUI : IEelementUI {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
