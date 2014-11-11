9b18a9e1cf92e8ed707470f4f8bdaaa95149d8b2
Arthur <arthur.devun@gmail.com>
2014-11-11 13:21:59 +0000

    Fix namespace

--- MediaMotion/Assets/Scripts/Core/Models/FileManager/Video.cs
+++ MediaMotion/Assets/Scripts/Core/Models/FileManager/Video.cs
@@ -2,7 +2,7 @@ using System;
 using MediaMotion.Core.Models.FileManager.Abstract;
 using MediaMotion.Core.Models.FileManager.Enums;
 
-namespace MediaMotion.Core.Models {
+namespace MediaMotion.Core.Models.FileManager {
 	public class Video : AFile {
 		public Video(string Path)
 			: base(Path) {
