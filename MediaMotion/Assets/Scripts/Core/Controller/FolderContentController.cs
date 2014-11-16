d123d09fa906a219b7774abc8bf22377ab1ac95a
Quentin Barate <barate.q@gmail.com>
2014-11-16 16:39:17 +0000

    Fixed glitch with folder presentation.

--- MediaMotion/Assets/Scripts/Core/Controller/FolderContentController.cs
+++ MediaMotion/Assets/Scripts/Core/Controller/FolderContentController.cs
@@ -14,8 +14,8 @@ namespace MediaMotion.Core.Controllers {
 		private int rowSize = 5;
 
 		/* Coordinates of the first placed tile */
-		private float originX = -2;
-		private float originZ = -1.5f;
+		private float originX = -3f;
+		private float originZ = -12.5f;
 
 		/* Value to increment to get the next IElement or next row */
 		private float incrementX = 1.5f;
@@ -31,8 +31,8 @@ namespace MediaMotion.Core.Controllers {
 			this.FileService = new FileSystem();
 			this.FileService.ChangeDirectory();
 			this.TextureMap = new Dictionary<ElementType, string>();
-			this.TextureMap.Add(ElementType.Folder, "File-icon.png");
-			this.TextureMap.Add(ElementType.File, "Folder-icon.png");
+			this.TextureMap.Add(ElementType.Folder, "File-icon");
+			this.TextureMap.Add(ElementType.File, "Folder-icon");
 			this.tiles = new List<GameObject>();
 			this.CurrentIndex = 0;
 		}
@@ -92,18 +92,25 @@ namespace MediaMotion.Core.Controllers {
 			List<IElement> content = this.FileService.GetDirectoryContent();
 			foreach (IElement file in content) {
 				GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Plane);
-				tile.transform.position.Set(x, 1, z);
-				tile.transform.rotation.Set(-40, 0, 0, -1);
+				tile.transform.position = new Vector3(x, 1, z);
+                tile.transform.eulerAngles = new Vector3(60, 180, 0);
+                tile.transform.localScale = new Vector3(0.115F, 0.1F, 0.1F);
 				//attribuer texture en fonction du fichier ?
+                Debug.Log(this.TextureMap[file.GetElementType()]);
 				tile.renderer.material.mainTexture = Resources.Load<Texture2D>(this.TextureMap[file.GetElementType()]);
 				//tile.AddComponent(COMPONENT_POUR_INFOS_FICHIER);
-				//tile.name = file.GetName();
+				tile.name = "Plane";
 				this.tiles.Add(tile);
 				++i;
+                if (i % this.rowSize == 0)
+                    x = this.originX;
+                else
+                    x += this.incrementX;
 				if (i % this.rowSize == 0)
 					z += this.incrementZ;
 			};
 		}
+
 
 		private void enterDirectory() {
 			this.FileService.ChangeDirectory(new Folder(this.tiles[this.CurrentIndex].name));
