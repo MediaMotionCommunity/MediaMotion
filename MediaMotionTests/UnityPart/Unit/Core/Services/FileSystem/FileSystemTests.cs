74a41abe27f7edc644cae68cebb6bd49ae77fbc4
Pierre-Etienne Cherière <pe.cheriere@gmail.com>
2014-11-21 16:36:03 +0000

    Add ChangeDirestory tests

--- MediaMotionTests/UnityPart/Unit/Core/Services/FileSystem/FileSystemTests.cs
+++ MediaMotionTests/UnityPart/Unit/Core/Services/FileSystem/FileSystemTests.cs
@@ -4,28 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MediaMotion.Core.Services.FileSystem;
+using MediaMotion.Core.Models.FileManager.Interfaces;
+using MediaMotion.Core.Models.FileManager;
+using MediaMotion.Core.Services.FileSystem.Interfaces;
+using MediaMotion.Core.Models.FileManager.Enums;
 using NUnit.Framework;
+
+
 namespace MediaMotion.Core.Services.FileSystem.Tests {
 	[TestFixture()]
 	public class FileSystemTests {
-		[Test()]
-		public void FileSystemTest() {
-			Assert.Fail();
+
+		public IFileSystem fileSystem;
+
+		[SetUp] public void Init() {
+			fileSystem = new FileSystem ();
 		}
 
 		[Test()]
-		public void GetWorkingDirectoryTest() {
-			Assert.Fail();
+		public void FileSystemTest() {
+			Assert.AreEqual (null, fileSystem);
 		}
 
 		[Test()]
 		public void GetHomeDirectoryTest() {
-			Assert.Fail();
+			String homePath = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
+			IFolder homeFolder = fileSystem.GetHomeDirectory();
+			fileSystem.ChangeDirectory (homeFolder);
+			Assert.AreEqual(homeFolder.GetName(), fileSystem.CurrentFolder.GetName());
+			Assert.AreEqual(homeFolder.GetPath(), fileSystem.CurrentFolder.GetPath());
+			Assert.AreEqual (homePath, fileSystem.CurrentFolder.GetPath ());
+			Assert.AreEqual (homePath, homeFolder.GetPath());
 		}
 
 		[Test()]
 		public void ChangeDirectoryTest() {
-			Assert.Fail();
+
+			// Testing valid values
+			IFolder newDirectory = null;
+			IFolder initialDirectory = fileSystem.GetHomeDirectory ();
+			fileSystem.ChangeDirectory (fileSystem.GetHomeDirectory());
+			Assert.AreEqual (ElementType.Folder, initialDirectory.GetElementType ());
+			List<IElement> directoryContent = fileSystem.GetDirectoryContent (fileSystem.CurrentFolder);
+			foreach (IElement element in directoryContent) {
+				if (element.GetElementType ().Equals(ElementType.Folder)) {
+					newDirectory = element as IFolder;
+					fileSystem.ChangeDirectory(element as IFolder);
+					break;
+				}
+			}
+			Assert.AreNotEqual (null, newDirectory);
+			Assert.AreNotEqual (newDirectory.GetPath (), initialDirectory.GetPath());
+			Assert.AreEqual (newDirectory.GetPath (), fileSystem.CurrentFolder.GetPath());
+
+
+			// Testing invalid value
+			IFolder invalidFolder = new Folder ("/invalid/path", "toto");
+			fileSystem.ChangeDirectory (invalidFolder); // Should return true/false ?
+			Assert.AreNotEqual (invalidFolder.GetPath (), fileSystem.CurrentFolder.GetPath ());
+			Assert.AreEqual (newDirectory.GetPath (), fileSystem.CurrentFolder.GetPath ());
+
+
+			// Add test with invalid folder like a IFile.
 		}
 
 		[Test()]
