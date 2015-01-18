bf55074315b5fe76b30e5462a39d40215b24ceda
Rafaël <rafael.caso2@gmail.com>
2015-01-17 20:08:57 -0500

    Add comment to PichSelectionDetection

--- WrapperDevices/MediaMotion.Motion.LeapMotion/MediaMotion.Motion.LeapMotion/MovementsDetection/PinchSelectionDetection.cs
+++ WrapperDevices/MediaMotion.Motion.LeapMotion/MediaMotion.Motion.LeapMotion/MovementsDetection/PinchSelectionDetection.cs
@@ -7,23 +7,49 @@ using System.Text;
 using MediaMotion.Motion.Actions;
 
 namespace MediaMotion.Motion.LeapMotion.MovementsDetection {
-	class PinchSelectionDetection : ICustomDetection {
-
+	/// <summary>
+	/// Class for pinch selection movement
+	/// </summary>
+	public class PinchSelectionDetection : ICustomDetection {
 		#region Constant
-
+		/// <summary>
+		/// Threshold for detection of pinch between 0-1
+		/// </summary>
 		private readonly float ThresholdDetection = 0.9f;
+		
+		/// <summary>
+		/// Maximun time for do and release pinch for valid detection
+		/// </summary>
 		private readonly TimeSpan ReleaseTimeMax = new TimeSpan(0, 0, 0, 0, 500);
 		#endregion
 
 		#region Fields
 
+		/// <summary>
+		/// Time of the last action generate
+		/// </summary>
 		private DateTime lastAction;
-		private Boolean detectionState;
+		
+		/// <summary>
+		/// State of detection global, use for avoid spamming select action
+		/// </summary>
+		private bool detectionState;
+
+		/// <summary>
+		/// Last detection of movements evolution
+		/// </summary>
 		private DateTime[] lastDetections;
-		private Boolean[] states;
+		
+		/// <summary>
+		/// Current states of movements evolution
+		/// </summary>
+		private bool[] states;
 		#endregion
 
 		#region Consturtor
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PinchSelectionDetection" /> class.
+		/// </summary>
 		public PinchSelectionDetection() {
 			this.lastAction = DateTime.Now;
 			this.detectionState = false;
@@ -32,16 +58,21 @@ namespace MediaMotion.Motion.LeapMotion.MovementsDetection {
 			this.lastDetections[0] = DateTime.Now;
 			this.lastDetections[1] = DateTime.Now;
 
-			this.states = new Boolean[2];
+			this.states = new bool[2];
 			this.states[0] = false;
 			this.states[1] = false;
 		}
 		#endregion
 
 		#region Methods
+		/// <summary>
+		/// Detection of PinchSelection
+		/// </summary>
+		/// <param name="frame">Leap Frame</param>
+		/// <returns>List of IAction</returns>
 		public IEnumerable<IAction> Detection(Frame frame) {
 			List<IAction> list = new List<IAction>();
-			Boolean detected = false;
+			bool detected = false;
 
 			if (this.detectionState) {
 				if (!frame.Hands.IsEmpty) {
@@ -62,11 +93,17 @@ namespace MediaMotion.Motion.LeapMotion.MovementsDetection {
 			return list;
 		}
 
-		private Boolean ValidDetection(int hand, float streng) {
-			if (streng < ThresholdDetection && this.states[hand]) {
+		/// <summary>
+		/// Check for valid detection of PinchSelection
+		/// </summary>
+		/// <param name="hand">int represent the hand use</param>
+		/// <param name="streng">float represent the streng of pinch</param>
+		/// <returns>true if the movment is valid</returns>
+		private bool ValidDetection(int hand, float streng) {
+			if (streng < this.ThresholdDetection && this.states[hand]) {
 				this.states[hand] = false;
 			}
-			if (streng >= ThresholdDetection && !this.states[hand]) {
+			if (streng >= this.ThresholdDetection && !this.states[hand]) {
 				this.states[hand] = true;
 				this.lastDetections[hand] = DateTime.Now;
 			} else if (!this.states[hand] && (DateTime.Now - this.lastDetections[hand] < this.ReleaseTimeMax)) {
