da622ac70f96d59d21f44937c6bca93bef8674d5
Maxfoow <maxime.charles@epitech.eu>
2015-01-18 10:50:00 -0500

    Add tdd library to nuget

--- MediaMotionTests/UnityPart/Unit/Core/Controller/FolderContentControllerTests.cs
+++ MediaMotionTests/UnityPart/Unit/Core/Controller/FolderContentControllerTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MediaMotion.Core.Controllers;
 using NUnit.Framework;
+
 namespace MediaMotion.Core.Controllers.Tests {
 	[TestFixture()]
 	public class FolderContentControllerTests {
