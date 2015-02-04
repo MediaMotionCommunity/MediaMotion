96c37c98d53c8541f804ef0ade44a02645cfac53
Arthur <arthur.devun@gmail.com>
2015-02-04 01:36:16 +0000

    Add default module

---
+++ MediaMotion/Assets/Project/Modules/DefaultViewer/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using MediaMotion.Core.Models.Scripts;
+
+namespace MediaMotion.Modules.DefaultViewer.Controllers {
+	/// <summary>
+	/// Default controller
+	/// </summary>
+	public class DefaultController : BaseUnityScript<DefaultController> {
+		/// <summary>
+		/// Initializes the specified module.
+		/// </summary>
+		/// <param name="module">The module.</param>
+		public void Init(DefaultViewerModule module) {
+			this.gameObject.guiText.text = ((module.Parameters.Length > 0) ? (module.Parameters[0].GetName()) : ("No parameter"));
+		}
+	}
+}
