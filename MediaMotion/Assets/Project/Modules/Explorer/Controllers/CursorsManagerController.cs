6c2927563befd74cf7b044c22fbd82dccf1207fd
Arthur <arthur.devun@gmail.com>
2015-03-08 19:07:01 +0000

    Add CursorManagerService, can disabled the selection of cursor

--- MediaMotion/Assets/Project/Modules/Explorer/Controllers/CursorsManagerController.cs
+++ MediaMotion/Assets/Project/Modules/Explorer/Controllers/CursorsManagerController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using MediaMotion.Core.Models.Scripts;
 using MediaMotion.Core.Services.Input.Interfaces;
+using MediaMotion.Modules.Explorer.Services.CursorManager.Interfaces;
 using MediaMotion.Motion.Actions;
 using UnityEngine;
 
@@ -16,11 +17,6 @@ namespace MediaMotion.Modules.Explorer.Controllers {
 		public const int DeleteDelay = 5;
 
 		/// <summary>
-		/// The main cursor tag
-		/// </summary>
-		public const string MainCursorTag = "MainCursor";
-
-		/// <summary>
 		/// The cursor prefabs
 		/// </summary>
 		public GameObject BaseCursor;
@@ -31,9 +27,9 @@ namespace MediaMotion.Modules.Explorer.Controllers {
 		private IInputService inputService;
 
 		/// <summary>
-		/// The cursors
+		/// The cursor manager service
 		/// </summary>
-		private Dictionary<int, CursorData> cursors;
+		private ICursorManagerService cursorManagerService;
 
 		/// <summary>
 		/// The explorer
@@ -44,9 +40,9 @@ namespace MediaMotion.Modules.Explorer.Controllers {
 		/// Initializes the specified input service.
 		/// </summary>
 		/// <param name="inputService">The input service.</param>
-		public void Init(IInputService inputService) {
+		public void Init(IInputService inputService, ICursorManagerService cursorManagerService) {
 			this.inputService = inputService;
-			this.cursors = new Dictionary<int, CursorData>();
+			this.cursorManagerService = cursorManagerService;
 			this.explorer = GameObject.Find("Explorer").GetComponent<ExplorerController>();
 		}
 
@@ -57,29 +53,18 @@ namespace MediaMotion.Modules.Explorer.Controllers {
 			foreach (IAction action in this.inputService.GetMovements(ActionType.BrowsingCursor)) {
 				int id = (action.Parameter as MediaMotion.Motion.Actions.Parameters.Object3).Id;
 
-				if (this.cursors.ContainsKey(id)) {
-					this.cursors[id].Update();
-				} else {
-					CursorData cursor = new CursorData(Instantiate(this.BaseCursor) as GameObject);
-					CursorController cursorController;
+				if (!this.cursorManagerService.UpdateCursor(id)) {
+					GameObject cursorObject = Instantiate(this.BaseCursor) as GameObject;
 
 					// init
-					cursor.GameObject.name = "cursor_" + id;
-					cursor.GameObject.transform.parent = this.gameObject.transform;
-					cursor.GameObject.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+					cursorObject.name = "cursor_" + id;
+					cursorObject.transform.parent = this.gameObject.transform;
+					cursorObject.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
 
-					// script
-					cursorController = cursor.GameObject.AddComponent<CursorController>();
-					cursorController.Id = id;
-					cursorController.Manager = this;
-
-					if (this.cursors.Count == 0) {
-						this.SetMainCursor(cursor);
-					}
-					this.cursors.Add(id, cursor);
+					this.cursorManagerService.AddCursor(cursorObject, id, this.Select, this.Deselect);
 				}
 			}
-			this.CleanCursors();
+			this.cursorManagerService.CleanCursors(DeleteDelay);
 		}
 
 		/// <summary>
@@ -88,7 +73,7 @@ namespace MediaMotion.Modules.Explorer.Controllers {
 		/// <param name="cursor">The cursor.</param>
 		/// <param name="gameObject">The game object.</param>
 		public void Select(GameObject cursor, GameObject gameObject) {
-			if (cursor.tag.CompareTo(MainCursorTag) == 0) {
+			if (this.cursorManagerService.IsMainCursor(cursor) && this.cursorManagerService.IsCursorEnabled(cursor)) {
 				this.explorer.Select(gameObject);
 			}
 		}
@@ -99,69 +84,8 @@ namespace MediaMotion.Modules.Explorer.Controllers {
 		/// <param name="cursor">The cursor.</param>
 		/// <param name="gameObject">The game object.</param>
 		public void Deselect(GameObject cursor, GameObject gameObject) {
-			if (cursor.tag.CompareTo(MainCursorTag) == 0) {
+			if (this.cursorManagerService.IsMainCursor(cursor) && this.cursorManagerService.IsCursorEnabled(cursor)) {
 				this.explorer.Deselect(gameObject);
-			}
-		}
-
-		/// <summary>
-		/// Sets the main cursor.
-		/// </summary>
-		/// <param name="cursor">The cursor.</param>
-		private void SetMainCursor(CursorData cursor) {
-			cursor.GameObject.tag = MainCursorTag;
-		}
-
-		/// <summary>
-		/// Cleans this instance.
-		/// </summary>
-		private void CleanCursors() {
-			List<int> toRemove = new List<int>();
-
-			foreach (KeyValuePair<int, CursorData> cursor in this.cursors.Where(cursorData => (cursorData.Value.LastFrameUpdate + DeleteDelay) < Time.frameCount)) {
-				GameObject.Destroy(cursor.Value.GameObject);
-				toRemove.Add(cursor.Key);
-			}
-			if (toRemove.Count > 0) {
-				foreach (int id in toRemove) {
-					this.cursors.Remove(id);
-				}
-
-				// if no more main cursor
-				if (!this.cursors.Any(cursorData => cursorData.Value.GameObject.tag.CompareTo(MainCursorTag) == 0) && this.cursors.Count > 0) {
-					this.SetMainCursor(this.cursors.First(cursorData => true).Value);
-				}
-			}
-		}
-
-		/// <summary>
-		/// Cursor data struct
-		/// </summary>
-		internal class CursorData {
-			/// <summary>
-			/// Initializes a new instance of the <see cref="CursorData"/> class.
-			/// </summary>
-			/// <param name="gameObject">The game object.</param>
-			public CursorData(GameObject gameObject) {
-				this.GameObject = gameObject;
-				this.LastFrameUpdate = Time.frameCount;
-			}
-
-			/// <summary>
-			/// The last frame update
-			/// </summary>
-			public int LastFrameUpdate { get; private set; }
-
-			/// <summary>
-			/// The game object
-			/// </summary>
-			public GameObject GameObject { get; private set; }
-
-			/// <summary>
-			/// Updates this instance.
-			/// </summary>
-			public void Update() {
-				this.LastFrameUpdate = Time.frameCount;
 			}
 		}
 	}
--- MediaMotion/Assets/Project/Modules/Explorer/ExplorerModule.cs
+++ MediaMotion/Assets/Project/Modules/Explorer/ExplorerModule.cs
@@ -3,6 +3,8 @@ using MediaMotion.Core.Models.Module.Abstracts;
 using MediaMotion.Core.Models.Module.Interfaces;
 using MediaMotion.Core.Models.Wrapper.Events;
 using MediaMotion.Core.Resolver.Containers.Interfaces;
+using MediaMotion.Modules.Explorer.Services.CursorManager;
+using MediaMotion.Modules.Explorer.Services.CursorManager.Interfaces;
 using MediaMotion.Motion.Actions;
 
 namespace MediaMotion.Modules.Explorer {
@@ -11,19 +13,29 @@ namespace MediaMotion.Modules.Explorer {
 	/// </summary>
 	public sealed class ExplorerModule : AModule {
 		/// <summary>
+		/// The builder
+		/// </summary>
+		private IContainerBuilder builder;
+
+		/// <summary>
 		/// Initializes a new instance of the <see cref="ExplorerModule"/> class.
 		/// </summary>
-		public ExplorerModule()
+		public ExplorerModule(IContainerBuilder builder)
 			: base() {
+			UnityEngine.Debug.Log(builder);
+			this.builder = builder;
 		}
 
 		/// <summary>
 		/// Configures this instance.
 		/// </summary>
-		protected override void Configure() {
+		public override void Configure() {
 			this.Configuration.Name = "File browser";
 			this.Configuration.Scene = "Explorer";
 			this.Configuration.Description = "File browser using the MediaMotion Core API";
+			this.Configuration.ServicesContainer = this.builder;
+
+			this.builder.Register<CursorManagerService>().As<ICursorManagerService>().SingleInstance();
 		}
 	}
 }
