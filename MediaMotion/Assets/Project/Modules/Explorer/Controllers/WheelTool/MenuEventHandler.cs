51a171a6be76a643fd5c78fcae1199d3496404bf
Quentin Barate <barate.q@gmail.com>
2015-05-16 16:35:48 +0100

    Popup commit.

--- MediaMotion/Assets/Project/Modules/Explorer/Controllers/WheelTool/MenuEventHandler.cs
+++ MediaMotion/Assets/Project/Modules/Explorer/Controllers/WheelTool/MenuEventHandler.cs
@@ -2,6 +2,7 @@
 using MediaMotion.Core.Services.FileSystem.Models.Interfaces;
 using MediaMotion.Core.Services.FileSystem.Models.Enums;
 using UnityEngine;
+using MediaMotion.Modules.Explorer.Controllers;
 
 /// <summary>
 /// from: <see href="https://github.com/leapmotion-examples/unity/tree/master/v1/freeform-menus">LeapMotion examples</see>
@@ -11,6 +12,11 @@ public class MenuEventHandler : MonoBehaviour {
 	/// The i
 	/// </summary>
 	private int i = 0;
+
+	/// <summary>
+	/// Explorer controller
+	/// </summary>
+	ExplorerController explorer;
 
 	/// <summary>
 	/// Receives the menu event.
@@ -23,11 +29,15 @@ public class MenuEventHandler : MonoBehaviour {
 		if (action.Equals(MenuBehavior.ButtonAction.OPEN) && selectedElement.GetElementType().Equals(ElementType.File)) {
 			System.Diagnostics.Process.Start(selectedElement.GetPath());
 		}
+		if (this.explorer.popupVisibility == false) {
+			this.explorer.popupVisibility = true;
+		}
 	}
 
 	/// <summary>
 	/// Starts this instance.
 	/// </summary>
 	private void Start() {
+		this.explorer = GameObject.Find("Explorer").GetComponent<ExplorerController>();
 	}
 }
